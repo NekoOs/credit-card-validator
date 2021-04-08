ea97fab5ab8cb345faffaf07a6dbedb82ba73926
David Barragan <hdavidbarragan@gmail.com>
2021-04-08 09:00:48 -0500

    Se agrega informacion en la documentacion, se habilita en la funcion isValidNumber consultar por numero y opcional tambien por tipo de tarjeta

--- src/Support/Facades/CreditCard.php
+++ src/Support/Facades/CreditCard.php
@@ -4,8 +4,41 @@ namespace NekoOs\Validator\Support\Facades;
 
 class CreditCard
 {
-    public static function isValid($number, $type)
+    /**
+     * Valida un numero de tarjeta de credito
+     * @param string $number numero de la tarjeta
+     * @param string $type franquicia de la tarjeta
+     *
+     * @return array: (valid, number, type).
+     * */
+    public static function isValidNumber($number, $type = null)
     {
-        return (bool)\Inacho\CreditCard::validCreditCard($number, $type)['valid'];
+        $separators = ['-', '.', ' ',','];
+        $number = str_replace($separators, '', trim((string)$number));
+        return \Inacho\CreditCard::validCreditCard($number);
+    }
+
+    /**
+     * Valida una fecha de una tarjeta
+     * @param string $date fecha en formato yyyy-mm
+     *
+     * @return boolean
+     * */
+    public static function isValidDate($date)
+    {
+        $date = explode('-', $date);
+        return (bool)\Inacho\CreditCard::validDate($date[0], $date[1]);
+    }
+
+    /**
+     * Valida una fecha de una tarjeta
+     * @param int $cvc codigo de seguridad de la tarjeta
+     * @param string $type franquicia de la tarjeta
+     *
+     * @return boolean
+     * */
+    public static function isValidCvc($cvc, $type)
+    {
+        return (bool)\Inacho\CreditCard::validCvc($cvc, $type);
     }
 }
